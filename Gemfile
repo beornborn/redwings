e9874ee6aa8dbff706fe839fd4bfcd9322853d7a
95kach <paliukh.mykhailo@gmail.com>
2015-06-17 15:47:08 +0300

    add_basic_authentication

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '4.2.1'
 
 gem 'pg'
+gem 'sorcery'
+gem 'bootstrap-sass', '3.2.0.0'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150613214301) do
+ActiveRecord::Schema.define(version: 20150617083938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,16 @@ ActiveRecord::Schema.define(version: 20150613214301) do
     t.string   "name"
     t.string   "lastname"
     t.string   "email"
-    t.boolean  "admin",      default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.boolean  "admin",                        default: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.string   "crypted_password"
+    t.string   "salt"
+    t.string   "remember_me_token"
+    t.datetime "remember_me_token_expires_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_me_token"], name: "index_users_on_remember_me_token", using: :btree
 
 end
