eec5fa0fe0892e9621c13ccf780b4bc066b10cb3
95kach <paliukh.mykhailo@gmail.com>
2015-06-20 15:44:48 +0300

    add template Session & PasswordReset controllers

---
+++ app/controllers/password_reset_controller.rb
@@ -0,0 +1,13 @@
+class PasswordResetController < ApplicationController
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,21 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/greeting'
 
+
+  get 'user/new'
+
+  get 'session/new'
+
+  get 'password_reset/new'
+  get 'password_reset/edit'
+
+  get  'login'  => 'session#new',     :as => :login
+  post 'logout' => 'session#destroy', :as => :logout
+
+  
+  resources :users
+  resources :sessions
+  resources :password_resets
+
 end
 
