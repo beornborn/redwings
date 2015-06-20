c834840bff702b2c97de99932e64d353c3828071
95kach <paliukh.mykhailo@gmail.com>
2015-06-20 15:31:24 +0300

    add template Session & PaswordReset controllers

---
+++ test/controllers/password_reset_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class PasswordResetControllerTest < ActionController::TestCase
+  test "should get create" do
+    get :create
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+
+  test "should get update" do
+    get :update
+    assert_response :success
+  end
+
+end
+
