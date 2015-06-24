1fbf48754f8d009b1b53a4b66404c4bfb2304524
95kach <paliukh.mykhailo@gmail.com>
2015-06-25 00:12:29 +0300

    add password_conformation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ActiveRecord::Base
   validates :name,     presence: true, length: { maximum: 50 }, format: { with: /[a-zA-Z]/ }
   validates :lastname, presence: true, length: { maximum: 50 }, format: { with: /[a-zA-Z]/ }
   validates :email,    presence: true
+
+  validates :password, confirmation: true, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
 end
 
