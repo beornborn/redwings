b03d2918545d924cc1d2ce7d573528479fd6b385
95kach <paliukh.mykhailo@gmail.com>
2015-06-25 13:43:15 +0300

    correct configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
   validates :name,     presence: true, length: { maximum: 50 }, format: { with: /[a-zA-Z]/ }
   validates :lastname, presence: true, length: { maximum: 50 }, format: { with: /[a-zA-Z]/ }
-  validates :email,    presence: true
+  validates :email,    presence: true, uniqueness: true
 
   validates :password, confirmation: true, length: { minimum: 4 }
   validates :password_confirmation, presence: true
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,8 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
-    port: '465',
-    domain: 'gmail.com',
+    port: '587',
+    domain: 'redwings.herokuapp.com',
     authentication: 'plain',
     enable_starttls_auto: true,
     user_name: 'redwings.ruby@gmail.com',
