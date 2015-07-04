b3981c93bc9601f0ea959ac475783349ceec5e82
95kach <paliukh.mykhailo@gmail.com>
2015-07-04 13:25:08 +0300

    deleting spaces

--- db/migrate/20150704091541_fix_database_columns.rb
+++ db/migrate/20150704091541_fix_database_columns.rb
@@ -1,8 +1,8 @@
 class FixDatabaseColumns < ActiveRecord::Migration
   def change
-  	rename_column :users, :name, :username
-  	rename_column :users, :lastname, :last_name
-  	add_column :users, :first_name, :string
+    rename_column :users, :name, :username
+    rename_column :users, :lastname, :last_name
+    add_column :users, :first_name, :string
   end
 end
 
