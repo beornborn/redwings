6751fa7e1dcb4cd6f7e698baec6dd2e5119e93af
95kach <paliukh.mykhailo@gmail.com>
2015-07-04 13:36:07 +0300

    deleting spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,26 +12,23 @@ class User < ActiveRecord::Base
   scope :deleted, -> (deleted) { where deleted: deleted }
 
 
-
   def User.slack_users
-  	data = Slack.get("https://ruby-redwings.slack.com/api/users.list")
-  	members = data["members"]
+    data = Slack.get("https://ruby-redwings.slack.com/api/users.list")
+    members = data["members"]
 
-  	users = []
-  	user = {}
+    users = []
+    user = {}
 
-    # filtered importent attr
-  	members.each do |member|
-      user["username"] = member["name"]
-      user["deleted"] = member["deleted"]
+    members.each do |member|
+      user["username"] =   member["name"]
+      user["deleted"] =    member["deleted"]
       user["first_name"] = member["profile"]["first_name"]
-      user["last_name"] = member["profile"]["last_name"]
-      user["image_48"] = member["profile"]["image_48"]
-      user["email"] = member["profile"]["email"]
-
-  	  users << user
-  	  user = {}
-  	end
+      user["last_name"] =  member["profile"]["last_name"]
+      user["image_48"] =   member["profile"]["image_48"]
+      user["email"] =      member["profile"]["email"]
+      users << user
+      user = {}
+    end
     users
   end
 
