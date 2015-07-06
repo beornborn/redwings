7d2e0ed9e50e392c92f38037dd6d696bd353d98c
Maxim Kachanov <m.b.kachanov@gmail.com>
2015-07-06 23:23:05 +0300

    implement create action

--- app/controllers/trello_backups_controller.rb
+++ app/controllers/trello_backups_controller.rb
@@ -4,12 +4,28 @@ class TrelloBackupsController < ApplicationController
   end
 
   def create
-
+    @boards = Trello::Board.all
+    @boards.each do |b|
+      board_backup = TrelloBackup.new(board: b.name,
+                       data: get_board_json(b.url, b.name))
+      if board_backup.save
+        redirect_to trello_backups_path
+      else
+        render.home
+    end
   end
 
   def destroy
   end
 
   private
+    def trello_backup_params
+      params.require(:trello_backup).permit(:board)
+    end
 
+    def get_board_json(url, name)
+      url_json = url.gsub("/#{name.dowcase}", ".json")
+      response = HTTParty.get(url_json)
+      JSON.parse(response.body)
+    end
 end
