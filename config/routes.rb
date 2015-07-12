3a4d7c20e9fe85fd8b178718113a3bff2d8e51ac
Maxim Kachanov <m.b.kachanov@gmail.com>
2015-07-12 23:53:01 +0300

    fix after rebase conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,15 @@ Rails.application.routes.draw do
   get  'login'  => 'user_sessions#new',     :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
-<<<<<<< HEAD
   namespace :services do
     resources :slack, only: [] do
       post :update_users, on: :collection
     end
   end
 
-  resources :users
-  resources :user_sessions
-  resources :password_resets
-=======
   resources :users, only: [:index]
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :trello_backups, only: [:index, :create, :destroy]
->>>>>>> thin down controller and wipe out unused routes
 end
 
