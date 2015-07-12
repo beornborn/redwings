79877a044c3a5723f0ddade142e37a5002e121ca
Maxim Kachanov <m.b.kachanov@gmail.com>
2015-07-12 22:39:07 +0300

    fix after rebase conflicts

--- config/application.rb
+++ config/application.rb
@@ -22,12 +22,8 @@ module Redwings
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-<<<<<<< HEAD
 
-    config.autoload_paths += %W(#{config.root}/lib)
-=======
     config.autoload_paths << Rails.root.join('lib')
->>>>>>> branch reload, check workability
   end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,21 +7,15 @@ Rails.application.routes.draw do
   get  'login'  => 'user_sessions#new',     :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
-<<<<<<< HEAD
   resources :users, only: [:index]
   resources :user_sessions, only: [:new, :create, :destroy]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :trello_backups, only: [:index, :create, :destroy]
-=======
+
   namespace :services do
     resources :slack, only: [] do
       post :update_users, on: :collection
     end
   end
-
-  resources :users
-  resources :user_sessions
-  resources :password_resets
->>>>>>> branch reload, check workability
 end
 
