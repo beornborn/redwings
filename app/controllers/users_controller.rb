c12b2fb5a1203a88be839976cbbf63127376ed94
95kach <paliukh.mykhailo@gmail.com>
2015-07-18 23:42:22 +0300

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   attr_accessor :skip_password_validation
 
   def index
-    @users = User.admin(false).deleted(false).order(created_at: :desc)
-    @users_deleted = User.admin(false).deleted(true).order(created_at: :desc)
+    @users = User.admin(false).deleted(false).order(started_at: :desc)
+    @users_deleted = User.admin(false).deleted(true).order(started_at: :desc)
   end
 
   def update
