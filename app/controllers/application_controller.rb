f56f64a1dbb241c49f0bed58021a778ef25e0faa
Boyko-Anna <boyko.anna.108@gmail.com>
2015-08-01 20:49:19 +0300

    Add draper gem and user decorator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :require_login
 
+  def current_user
+   UserDecorator.decorate(super) unless super == false
+  end
+
   private
 
   def not_authenticated
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
   attr_accessor :skip_password_validation
 
   def index
-    @users = User.where.not(id: current_user.id).admin(false).deleted(false).order(started_at: :desc)
-    @users_deleted = User.admin(false).deleted(true).order(started_at: :desc)
+    @users = User.where.not(id: current_user.id).admin(false).deleted(false).order(started_at: :desc).decorate
+    @users_deleted = User.admin(false).deleted(true).order(started_at: :desc).decorate
   end
 
   def update
