6b4e0ef7822e7c04437c16512f1a98e99605a932
Boyko-Anna <boyko.anna.108@gmail.com>
2015-08-02 00:37:44 +0300

    Rename defs in user_decorator, add trello_backup_decorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,22 +1,11 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
-  decorates :user
-
-  def name
+  def full_name
     object.first_name + ' ' + object.last_name
   end
 
-  def started_at
+  def human_started_at
     object.started_at.present? && object.started_at.strftime('%e %B %Y')
   end
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
 end
