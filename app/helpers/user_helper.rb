294c53ca997ae6cb3a87666387bb72ca37c9f08c
95kach <paliukh.mykhailo@gmail.com>
2015-08-04 22:40:58 +0300

    fixing and refactoring

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,10 @@
+module UserHelper
+  def academy_users_count
+  	Project.find_by(name: 'Academy').users.deleted(false).count
+  end
+
+  def redwings_users_count
+  	Project.find_by(name: 'Redwings').users.deleted(false).count
+  end
+end
+
