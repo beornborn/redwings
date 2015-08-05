e141ca991f3a5f81b1a5799d309462745ca5e9b4
95kach <paliukh.mykhailo@gmail.com>
2015-08-06 00:55:03 +0300

    refactoring

--- app/controllers/service/herokus_controller.rb
+++ app/controllers/service/herokus_controller.rb
@@ -5,20 +5,3 @@ class Service::HerokusController < ApplicationController
   end
 end
 
-class Database
-  def self.sync
-    puts 'Syncing local development database with production...'
-
-    db_config = Rails.configuration.database_configuration
-    database_name = db_config['development']['database']
-
-    begin
-      Bundler.with_clean_env {`heroku pg:backups capture`}
-      Bundler.with_clean_env {`curl -o latest.dump \`heroku pg:backups public-url\``}
-      `pg_restore --verbose --clean --no-acl --no-owner --jobs=2 -n public -d #{database_name} latest.dump`
-    ensure
-      `rm latest.dump`
-    end
-  end
-end
-
