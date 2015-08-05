b97b1a07503304066e6afc1c8abb2baf8e5f268b
95kach <paliukh.mykhailo@gmail.com>
2015-08-05 20:08:54 +0300

    delete spaces

--- app/controllers/service/herokus_controller.rb
+++ app/controllers/service/herokus_controller.rb
@@ -2,8 +2,8 @@ require 'rake'
 
 class Service::HerokusController < ApplicationController
   def sync
-  	%x(bundle exec rake db:sync)
-  	redirect_to root_path, flash: { success: 'Local database successfully updates!' }
+    %x(bundle exec rake db:sync)
+    redirect_to root_path, flash: { success: 'Local database successfully updates!' }
   end
 end
 
--- app/controllers/service/slacks_controller.rb
+++ app/controllers/service/slacks_controller.rb
@@ -1,11 +1,11 @@
 class Service::SlacksController < ApplicationController
   def sync
-  	if Rails.env.production?
-  	  Service::Slack.sync
-  	  redirect_to root_path
-  	else
-  	  redirect_to root_path, flash: { danger: "You can't do this from local machine!" }
-  	end
+    if Rails.env.production?
+      Service::Slack.sync
+      redirect_to root_path
+    else
+      redirect_to root_path, flash: { danger: "You can't do this from local machine!" }
+    end
   end
 end
 
