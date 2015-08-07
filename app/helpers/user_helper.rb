3e5c5bd99d14391234ceb2f48d74f2d274670cf5
95kach <paliukh.mykhailo@gmail.com>
2015-08-07 12:19:08 +0300

    remove spaces

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,6 +1,6 @@
 module UserHelper
   def link_to_users(filter)
-  	link = link_to filter.capitalize, users_path(filter: filter)
+    link = link_to filter.capitalize, users_path(filter: filter)
     current_page?(users_path(filter: filter)) ? content_tag(:li, link, class: 'active') : content_tag(:li, link)
   end
 end
